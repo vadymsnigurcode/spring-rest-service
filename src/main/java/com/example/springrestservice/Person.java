dc5d3201bf5846078a92b4e56dc16c5851e1a6b6
Vadym <vadym.snigur.code@gmail.com>
2022-02-09 20:58:32 +0200

    added people data support

---
+++ src/main/java/com/example/springrestservice/Person.java
@@ -0,0 +1,20 @@
+package com.example.springrestservice;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+}
